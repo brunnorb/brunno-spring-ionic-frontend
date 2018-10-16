98cdfb9251ce11ea30ab0caa3e5a351590122d15
Brunno Ribeiro <brunnorb@gmail.com>
2018-10-16 11:25:13 -0300

    Infinit scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -11,7 +11,9 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class ProdutosPage {
 
-  items: ProdutoDTO[];
+  items: ProdutoDTO[] = [];
+
+  page : number = 0;
 
   constructor(
       public navCtrl: NavController, 
@@ -21,7 +23,19 @@ export class ProdutosPage {
       ) {
   }
 
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+
+    setTimeout(() => {
+      infiniteScroll.complete();
+    }, 1000);
+  }
+
+
   doRefresh(refresher) {
+    this.page = 0;
+    this.items = [];
     this.loadData();
     setTimeout(() => {
       refresher.complete();
@@ -44,10 +58,12 @@ export class ProdutosPage {
   loadData(){
     let categoriaId = this.navParams.get('categoriaId');
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoriaId)
+    this.produtoService.findByCategoria(categoriaId,this.page, 10)
       .subscribe(response => {
-                this.items = response['content'];
-                this.loadImageUrls();
+                let start = this.items.length;
+                this.items = this.items.concat(response['content']);
+                let end = this.items.length-1;
+                this.loadImageUrls(start,end);
                 loader.dismiss();
 
       },
@@ -57,8 +73,8 @@ export class ProdutosPage {
 
   }
 
-  loadImageUrls(){
-    for (var i=0; i<this.items.length; i++) {
+  loadImageUrls(start:number,end:number){
+    for (var i=start; i<=end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
           .subscribe(response => {
