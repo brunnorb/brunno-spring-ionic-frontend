0a9ab27d5debf1fa985c929037436da2a4e157b1
Brunno Ribeiro <brunnorb@gmail.com>
2018-10-16 10:58:28 -0300

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
@@ -23,19 +23,32 @@ export class ProdutosPage {
   constructor(
       public navCtrl: NavController, 
       public navParams: NavParams,
-      public produtoService: ProdutoService
+      public produtoService: ProdutoService,
+      public loadingCtrl: LoadingController
       ) {
   }
 
+  presentLoading(){
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader;
+  }
+
   ionViewDidLoad() {
-    let categoriaId = this.navParams.get('categoriaId')
+    let categoriaId = this.navParams.get('categoriaId');
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoriaId)
       .subscribe(response => {
                 this.items = response['content'];
                 this.loadImageUrls();
+                loader.dismiss();
 
       },
-      error => {});
+      error => {
+        loader.dismiss();
+      });
   }
 
   loadImageUrls(){
